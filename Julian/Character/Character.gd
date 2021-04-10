ec849532ffefc324e1834c7a114d2ff095116645
jaacko-torus <jaacko.torus@gmail.com>
2021-04-10 12:49:53 -0400

    character can move and jump

--- Julian/Character/Character.gd
+++ Julian/Character/Character.gd
@@ -4,23 +4,24 @@ var run_speed = 350
 var jump_speed = -1000
 var gravity = 2500
 
-var velocity = Vector2()
+var velocity = Vector2.ZERO
+
+var input = {
+	movement = Vector2.ZERO,
+	jump = false
+}
 
 func get_input():
-	velocity.x = 0
+	self.input.movement.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
 	
-	var right = Input.is_action_pressed("right")
-	var left = Input.is_action_pressed("left")
-	var jump = Input.is_action_just_pressed("jump")
+	self.input.jump = Input.is_action_just_pressed("jump")
 	
-	if is_on_floor() and jump:
-		velocity.y = jump_speed
-	if right:
-		velocity.x += run_speed
-	if left:
-		velocity.x -= run_speed
+	if self.is_on_floor() and self.input.jump:
+		self.velocity.y = self.jump_speed
+	
+	self.velocity.x = self.input.movement.x * self.run_speed
 
 func _physics_process(delta):
-	velocity.y += gravity * delta
-	get_input()
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+	self.velocity.y += self.gravity * delta
+	self.get_input()
+	self.velocity = self.move_and_slide(self.velocity, Vector2.UP)
