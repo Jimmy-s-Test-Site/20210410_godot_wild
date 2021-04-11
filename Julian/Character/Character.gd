689c3609121566c736f94bd9c813c457402a964d
jaacko-torus <jaacko.torus@gmail.com>
2021-04-11 18:17:37 -0400

    added laser

--- Julian/Character/Character.gd
+++ Julian/Character/Character.gd
@@ -1,8 +1,10 @@
 extends KinematicBody2D
 
-var run_speed = 350
-var jump_speed = -1000
-var gravity = 2500
+export(int) var run_speed = 350
+export(int) var jump_speed = -1000
+export(int) var gravity = 2500
+
+var alive = true
 
 var velocity = Vector2.ZERO
 
@@ -11,17 +13,40 @@ var input = {
 	jump = false
 }
 
-func get_input():
-	self.input.movement.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+func _physics_process(delta : float) -> void:
+	if alive:
+		self.input_manager()
+		self.movement_manager(delta)
+		self.attack_manager()
+		self.death_manager()
+		self.animation_manager()
+
+func input_manager() -> void:
+	self.input.movement = Vector2(
+		int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left")),
+		int(Input.is_action_pressed("up"))    - int(Input.is_action_pressed("down")))
 	
 	self.input.jump = Input.is_action_just_pressed("jump")
+
+func movement_manager(delta : float) -> void:
+	# add gravity
+	self.velocity.y += self.gravity * delta
 	
+	# add jump force
 	if self.is_on_floor() and self.input.jump:
 		self.velocity.y = self.jump_speed
 	
+	# add horizontal movement
 	self.velocity.x = self.input.movement.x * self.run_speed
-
-func _physics_process(delta):
-	self.velocity.y += self.gravity * delta
-	self.get_input()
+	
+	# apply movement
 	self.velocity = self.move_and_slide(self.velocity, Vector2.UP)
+
+func attack_manager():
+	pass
+
+func death_manager():
+	pass
+
+func animation_manager():
+	pass
