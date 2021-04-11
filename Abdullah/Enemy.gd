ceea99b0d303476de6b739cc6c00574880118395
Fenguinn <salem.abdulla15@gmail.com>
2021-04-10 22:02:08 -0400

    Mario Simulation(-3.4)

--- Abdullah/Enemy.gd
+++ Abdullah/Enemy.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
 signal died
+signal ready_hit
+signal hit_player
 
 export (NodePath) var player_character_path
 
@@ -10,11 +12,12 @@ export (int) var  movement_speed = 100
 export (int) var jump_speed
 
 export (int) var attack_power
-export (int) var health
+export (int) var health = 1
 
 export (int) var gravity = 250
 
 var velocity = Vector2.ZERO
+var is_alive = true
 var direction_to_move
 
 func movement_manager(delta : float):
@@ -33,6 +36,23 @@ func movement_manager(delta : float):
 	
 	self.velocity = self.move_and_slide(self.velocity, Vector2.UP)
 
+func health_manager():
+	if health <= 0:
+		emit_signal("died")
+		#$CollisionShape2D.disabled = true
+		self.is_alive = false
+		#self.visible = false
+		self.queue_free()
+
+func attack_manager():
+	if direction_to_move > (100) or direction_to_move < (-100):
+		$"Attack Area/Range".disabled = true
+	else:
+		$"Attack Area/Range".disabled = false
+
+func animation_manager():
+	pass
+
 func find_character():
 	self.direction_to_move = self.position.x - player_character.position.x
 
@@ -42,5 +62,25 @@ func _ready():
 
 
 func _process(delta):
-	self.find_character()
-	self.movement_manager(delta)
+	self.health_manager()
+	if self.is_alive:
+		self.find_character()
+		self.movement_manager(delta)
+		self.attack_manager()
+	
+
+
+func _on_Attack_Area_body_entered(body):
+	if body.name == "Character":
+		emit_signal("hit_player")
+
+
+
+
+
+
+
+func _on_HitBox_body_entered(body):
+	if body.name == "Character":
+		var damage = 1#body.damage
+		self.health -= 1
