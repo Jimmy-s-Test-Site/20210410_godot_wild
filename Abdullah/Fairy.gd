5bb4e1f03c59c34a357851783390952a0a878a7f
jaacko-torus <jaacko.torus@gmail.com>
2021-04-14 20:55:37 -0400

    Fairy follows player

--- Abdullah/Fairy.gd
+++ Abdullah/Fairy.gd
@@ -1,9 +1,40 @@
-extends Node2D
+extends KinematicBody2D
 
 signal Fire_Pillar
 
+export(NodePath) var player_path
+export(NodePath) var spawn_points_path
+export(int) var speed = 4000
 
+var casting
+var facing_direction = 1
 
-# Called when the node enters the scene tree for the first time.
+func get_player() -> Object:
+	return get_node_or_null(self.player_path)
+
+func get_spawn_point() -> Object:
+	return get_node_or_null(self.spawn_points_path)
+
 func _ready():
-	pass
+	$AnimationPlayer.play("casting_right")
+
+var movement = Vector2.ZERO
+
+func _physics_process(delta : float) -> void:
+	self.movement_manager(delta)
+	self.animation_manager()
+
+func movement_manager(delta : float) -> void:
+	if self.get_player() != null:
+		var direction_to_move = (self.get_player().position - self.position).normalized()
+		self.facing_direction = sign(direction_to_move.x)
+		
+		self.movement = direction_to_move
+		
+		self.movement = self.move_and_slide(self.movement * self.speed * delta)
+
+func animation_manager():
+	if self.facing_direction == 1:
+		$AnimationPlayer.play("casting_right")
+	else:
+		$AnimationPlayer.play("casting_left")
