b7c7a183f6d8d7a80dc6a4e98892d32d8663ec22
jaacko-torus <jaacko.torus@gmail.com>
2021-04-16 00:13:03 -0400

    finished level transitions

--- Julian/Game/Game.gd
+++ Julian/Game/Game.gd
@@ -2,23 +2,22 @@ extends Node2D
 
 export(Array, PackedScene) var levels
 
-var scene_index = -1
-var scene : Node2D = null
+var level_index = -1
+var level : Node2D = null
 
 func _ready():
-	self.load_scene(0)
+	self.load_level(0)
 
-func load_scene(n : int) -> void:
-	if self.scene != null:
-		self.scene.disconnect("goto_next", self, "on_level_goto_next")
-		self.scene.queue_free()
+func load_level(n : int) -> void:
+	if self.level != null:
+		self.level.disconnect("goto_next", self, "on_level_goto_next")
+		self.level.queue_free()
 	
-	self.scene_index = n
-	self.scene = self.levels[n].instance()
+	self.level_index = n
+	self.level = self.levels[n].instance()
+	self.level.connect("goto_next", self, "on_level_goto_next")
 	
-	self.scene.connect("goto_next", self, "on_level_goto_next")
-	
-	self.add_child(scene)
+	self.call_deferred("add_child", self.level)# add_child(level)
 
 func on_level_goto_next() -> void:
-	self.load_scene((self.scene_index + 1) % self.levels.length)
+	self.load_level(self.level_index + 1)
