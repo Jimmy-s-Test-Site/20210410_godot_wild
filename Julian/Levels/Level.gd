26c9bdc6106591f9b4ce202369385bf454d63e66
jaacko-torus <jaacko.torus@gmail.com>
2021-04-16 00:14:14 -0400

    level transition logic that has to do with level transitions

--- Julian/Levels/Level.gd
+++ Julian/Levels/Level.gd
@@ -2,5 +2,10 @@ extends Node2D
 
 signal goto_next
 
+export(NodePath) var slot_machine_path = null
+
 func _ready():
 	$AudioStreamPlayer.play()
+
+func _on_SlotMachine_teleport():
+	self.emit_signal("goto_next")
