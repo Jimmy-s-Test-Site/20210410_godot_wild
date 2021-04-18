52d5a06e133cf67ee7483d95e072e82b2b63875c
jaacko-torus <jaacko.torus@gmail.com>
2021-04-18 00:18:01 -0400

    added goto_next to StartScreen scene

---
+++ Abdullah/Start Screen.gd
@@ -0,0 +1,21 @@
+extends Control
+
+signal goto_next
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Label_goto_next():
+	self.emit_signal("goto_next")
